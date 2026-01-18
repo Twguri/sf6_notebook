bbf0a857e8e9bea4d7d9f170ba457e119390eae8
Twguri <thomaswei3213@gmail.com>
2026-01-18 11:49:18 -0500

    update vite base

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,8 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
-// https://vite.dev/config/
 export default defineConfig({
+  base: "/sf6_notebook/",
   plugins: [react()],
-  base:"/sf6_notebook/",
-})
+});
+
